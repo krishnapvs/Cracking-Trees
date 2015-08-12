3d5b0f2aefc00ac7a6d052ebb361290bf77f908d
Krishna VS Puvvada <vpuvvad@ncsu.edu>
2015-08-11 20:29:20 -0400

    Update 4.5.py

--- 4.5.py
+++ 4.5.py
@@ -19,19 +19,23 @@ class BinTree:
                 self.right=BinTree(key)
 
 def isBinarySearchTree(tree):
-    flag=False
+    ''' Function to check if the tree is a binary search tree. Returns True if the tree is binarySearchTree else returns False'''
+    # case when no child nodes are present
     if (not (tree.left) and not (tree.right)):
         return True
+    # case when only right child is present
     elif not tree.left:
         if tree.right.key <= tree.key:
             return False
         else:
             return isBinarySearchTree(tree.right)
+    # case when only left child is present
     elif not tree.right:
         if tree.left.key > tree.key:
             return False
         else:
             return isBinarySearchTree(tree.left)
+    # case when both the children are present
     else:
         return (tree.left.key <= tree.key < tree.right.key) and isBinarySearchTree(tree.left) and isBinarySearchTree(tree.right)
 
